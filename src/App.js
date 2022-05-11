d70371711aec8313b26485156cc386b8aaaa1d0a
nlopez.developer@outlook.com <nlopez.developer@outlook.com>
2022-05-11 17:48:46 -0300

    Subida de nuevo a git components con navbar

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import './App.css';
+import NavBar from './components/NavBar';
 
 function App() {
   return (
     <div className="App">
+      <NavBar></NavBar>
       <h1>Mi Patisserie</h1>
       <h2>By ReactJS</h2>
     </div>
